2b52cf9676ab78e6d17260653a06eaa8ad037e84
Fabio Forni <fabio@redaril.me>
2024-02-14 12:24:06 +0100

    stone1: Prepare Reader.NextRecord method

--- stone1/reader.go
+++ stone1/reader.go
@@ -7,20 +7,24 @@ type Reader struct {
 	pre Prelude
 	src io.Reader
 
+	currHeader Header
 	idxPayload int
+	idxRecord  int
 }
 
 // NewReader creates a new Reader.
 func NewReader(pre Prelude, src io.Reader) *Reader {
 	return &Reader{
-		pre: pre,
-		src: src,
+		pre:        pre,
+		src:        src,
+		idxPayload: -1,
+		idxRecord:  -1,
 	}
 }
 
-// Next advances to the next Header. It returns an empty Header
-// and io.EOF if it reached the end of stone archive.
-func (r *Reader) Next() (Header, error) {
+// NextPayload advances to the next payload Header.
+// It returns an empty Header and io.EOF if it reached the end of stone archive.
+func (r *Reader) NextPayload() (Header, error) {
 	if r.idxPayload >= int(r.pre.NumPayloads) {
 		return Header{}, io.EOF
 	}
@@ -28,8 +32,25 @@ func (r *Reader) Next() (Header, error) {
 	if err != nil {
 		return Header{}, err
 	}
+	r.currHeader = hdr
 	r.idxPayload += 1
+	r.idxRecord = 0
 	return hdr, nil
+}
+
+// NextRecord advances to the next payload record.
+// It returns a nil record and io.EOF if it reached the end of the current payload.
+// It panics if NextPayload was not called beforehand.
+func (r *Reader) NextRecord() (any, error) {
+	if r.idxPayload < 0 {
+		panic("NextPayload was not called")
+	}
+	if r.idxRecord >= int(r.currHeader.NumRecords) {
+		return nil, io.EOF
+	}
+	// TODO: Read.
+	r.idxRecord += 1
+	return nil, nil
 }
 
 func (r *Reader) readHeader() (Header, error) {
